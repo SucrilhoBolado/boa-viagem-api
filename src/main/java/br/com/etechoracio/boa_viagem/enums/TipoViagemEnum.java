2c35dae7d1ec6e8a3f0070bb0d47a6b13b3b6505
rodrigo-bruno <Rodrigo-Rodrigofreitas2568@outlook.com.br>
2022-03-23 14:07:39 -0300

    Criacao de enumeracoes

---
+++ src/main/java/br/com/etechoracio/boa_viagem/enums/TipoViagemEnum.java
@@ -0,0 +1,16 @@
+package br.com.etechoracio.boa_viagem.enums;
+
+public enum TipoViagemEnum {
+LAZER,
+NEGOCIOS;
+	
+public enum CategoriaEnum {
+		ALIMENTACAO,
+		COMBUSTIVEL,
+		TRANSPORTE,
+		HOSPEDAGEM,
+		OUTROS;
+		}
+}
+
+
---
+++ src/main/java/br/com/etechoracio/boa_viagem/enums/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Aluno
+ *
+ */
+package br.com.etechoracio.boa_viagem.enums;
